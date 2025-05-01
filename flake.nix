d3dbb04aa6800d8c71d3c2cb885197f160abb151
installer <installer@gmail.com>
2025-05-01 10:38:26 -0500

    distrobox added

--- flake.nix
+++ flake.nix
@@ -1,40 +1,41 @@
 {
-  inputs = {
-    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
-    systems.url = "github:nix-systems/default";
-    devenv.url = "github:cachix/devenv";
-    devenv.inputs.nixpkgs.follows = "nixpkgs";
-    fenix.url = "github:nix-community/fenix";
-    fenix.inputs = { nixpkgs.follows = "nixpkgs"; };
-    nixpkgs-python.url = "github:cachix/nixpkgs-python";
-    nixpkgs-python.inputs = { nixpkgs.follows = "nixpkgs"; };
-  };
+    inputs = {
+        nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+        systems.url = "github:nix-systems/default";
+        devenv.url = "github:cachix/devenv";
+        devenv.inputs.nixpkgs.follows = "nixpkgs";
+        fenix.url = "github:nix-community/fenix";
+        fenix.inputs = { nixpkgs.follows = "nixpkgs"; };
+        nixpkgs-python.url = "github:cachix/nixpkgs-python";
+        nixpkgs-python.inputs = { nixpkgs.follows = "nixpkgs"; };
+    };
 
-  nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
-  };
+    nixConfig = {
+        extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+        extra-substituters = "https://devenv.cachix.org";
+    };
 
-  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
-    let
-      forEachSystem = nixpkgs.lib.genAttrs (import systems);
-    in
-    {
-      packages = forEachSystem (system: {
-        devenv-up = self.devShells.${system}.default.config.procfileScript;
-        devenv-test = self.devShells.${system}.default.config.test;
-      });
+    outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+        let
+            forEachSystem = nixpkgs.lib.genAttrs (import systems);
+        in
+            {
+            packages = forEachSystem (system: {
+                devenv-up = self.devShells.${system}.default.config.procfileScript;
+                devenv-test = self.devShells.${system}.default.config.test;
+            });
 
-      devShells = forEachSystem
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-          in
-          {
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [
-                {
+            devShells = forEachSystem
+                (system:
+                    let
+                        pkgs = nixpkgs.legacyPackages.${system};
+                    in
+                        {
+                        default = devenv.lib.mkShell {
+                            inherit inputs pkgs;
+                            modules = [
+                                {
+                                    packages = [ pkgs.distrobox ];
                                     # https://devenv.sh/reference/options/
                                     languages.python = {
                                         enable = true;
@@ -72,12 +73,11 @@
                                         targets = [ "wasm32-unknown-unknown" ];
                                     };
 
-                                    languages.julia = {
-                                        enable = true;
-                                    };
+                                    enterShell = ''
+                                      julia -e 'using Pkg; try using IJulia catch; Pkg.add("IJulia"); end'
+                                    '';
 
-
-                                    processes.hello.exec = "hello";
+                                    # processes.hello.exec = "hello";
                                 }
                             ];
                         };
